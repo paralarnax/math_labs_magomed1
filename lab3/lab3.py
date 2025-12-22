6889e9ca0505f7f500a5a70e853aac084424d2e0
semk-redfab <semk@redfab.ru>
2025-12-23 02:33:48 +0300

    lab3 fix

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,35 +1,35 @@
 import numpy as np
 
 def get_matrix(link_length_a, link_twist_alpha, link_offset_d, joint_angle_theta):
-    try:
-        a = float(link_length_a)
-        alpha = float(link_twist_alpha)
-        d = float(link_offset_d)
-        theta = float(joint_angle_theta)
 
-        T = np.array([
-            [np.cos(theta), -np.sin(theta) * np.cos(alpha), np.sin(theta) * np.sin(alpha), a * np.cos(theta)],
-            [np.sin(theta), np.cos(theta) * np.cos(alpha), -np.cos(theta) * np.sin(alpha), a * np.sin(theta)],
-            [0, np.sin(alpha), np.cos(alpha), d],
-            [0, 0, 0, 1]
-        ])
+    a = float(link_length_a)
+    alpha = float(link_twist_alpha)
+    d = float(link_offset_d)
+    theta = float(joint_angle_theta)
+    T = np.array([
+        [np.cos(theta), -np.sin(theta) * np.cos(alpha), np.sin(theta) * np.sin(alpha), a * np.cos(theta)],
+        [np.sin(theta), np.cos(theta) * np.cos(alpha), -np.cos(theta) * np.sin(alpha), a * np.sin(theta)],
+        [0, np.sin(alpha), np.cos(alpha), d],
+        [0, 0, 0, 1]
+    ])
+    T = np.round(T, 3)
+    return T
 
-        T = np.round(T, 3)
-
-        return T
-
-    except (TypeError, ValueError):
-        print("Ne chisla")
-        return None
 
 
 def get_tool_pose(O1, O2, O3, O4, O5, g):
+    # Прописываем длины звеньев
     a = 3
     b = 5.75
     c = 7.375
     d = 4.125
     e = 1.125
 
+    # Получаем матрицы преобразования по параметрам денавита хартенберга
+    # длина звена
+    # скручивание звена
+    # смещение вдоль шарнира
+    # угол поворота шарнира
     T0 =  get_matrix(0, 0, 0, 0)
     T01 = get_matrix(0, -np.pi / 2, a, O1)
     T12 = get_matrix(b, 0, 0, O2 - np.pi / 2)
@@ -69,16 +69,16 @@ def get_tool_pose(O1, O2, O3, O4, O5, g):
     return result
 
 if __name__ == '__main__':
-    print("task1")
+    print("Задание 1 - матрица преобразования Денавита-Хартенберга")
     print(get_matrix(5, 0, 3, np.pi/2))
 
     answer_2_1 = get_tool_pose(0, 0, 0, 0, 0, 0)
 
-    print("\ntask2 part1")
+    print("Задание 2 часть 1 (x, y, z)")
     for item in answer_2_1:
         print(item)
 
-    print("\ntask2 part2")
+    print("Задание 2 часть 2 (x, y, z)")
     answer_2_2 = get_tool_pose(np.pi, np.pi/2, np.pi/2, -np.pi/2, -np.pi/6, 2)
     for item in answer_2_2:
         print(item)
